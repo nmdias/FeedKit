5c5eb5e4826f78479a194d9da7e8210519075f89
Nuno Dias <nmdias.pt@gmail.com>
2016-05-15 09:37:02 +0100

    Added Iris macOS Framework target

---
+++ Iris macOS/Iris macOS.h
@@ -0,0 +1,19 @@
+//
+//  Iris macOS.h
+//  Iris macOS
+//
+//  Created by Nuno Dias on 15/05/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Iris macOS.
+FOUNDATION_EXPORT double Iris_macOSVersionNumber;
+
+//! Project version string for Iris macOS.
+FOUNDATION_EXPORT const unsigned char Iris_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iris_macOS/PublicHeader.h>
+
+
