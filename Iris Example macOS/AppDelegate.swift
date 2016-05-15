1731ec4d70170cf89c365d09ceb70558e21de2a7
Nuno Dias <nmdias.pt@gmail.com>
2016-05-15 09:29:23 +0100

    Added macOS Example target

---
+++ Iris Example macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Iris Example macOS
+//
+//  Created by Nuno Dias on 15/05/16.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
