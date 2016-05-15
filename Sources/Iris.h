bd46212797363d690f854bb4f4025461b4efdbf4
Nuno Dias <nmdias.pt@gmail.com>
2016-05-15 09:51:21 +0100

    Refactored the multiple OS's target folders to a single Sources folder

---
+++ Sources/Iris.h
@@ -0,0 +1,19 @@
+//
+//  Iris.h
+//  Iris
+//
+//  Created by Nuno Dias on 15/05/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Iris.
+FOUNDATION_EXPORT double IrisVersionNumber;
+
+//! Project version string for Iris.
+FOUNDATION_EXPORT const unsigned char IrisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iris/PublicHeader.h>
+
+
