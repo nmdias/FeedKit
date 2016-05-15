c68b5ea5a431b05b6ac203032735ab859e6238f5
Nuno Dias <nmdias.pt@gmail.com>
2016-05-15 09:36:29 +0100

    Added Iris tvOS Framework target

---
+++ Iris tvOS/Iris tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Iris tvOS.h
+//  Iris tvOS
+//
+//  Created by Nuno Dias on 15/05/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Iris tvOS.
+FOUNDATION_EXPORT double Iris_tvOSVersionNumber;
+
+//! Project version string for Iris tvOS.
+FOUNDATION_EXPORT const unsigned char Iris_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iris_tvOS/PublicHeader.h>
+
+
