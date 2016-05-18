e6736d9b78e9f4bf31091adaf0127502ffb9f543
Nuno Dias <nmdias.pt@gmail.com>
2016-05-18 08:25:21 +0100

    Renamed and updated the Public Header to FeedParser.h

---
+++ Sources/FeedParser.h
@@ -0,0 +1,19 @@
+//
+//  FeedParser.h
+//  FeedParser
+//
+//  Created by Nuno Dias on 15/05/16.
+//
+//
+
+@import Foundation;
+
+//! Project version number for FeedParser.
+FOUNDATION_EXPORT double FeedParserVersionNumber;
+
+//! Project version string for FeedParser.
+FOUNDATION_EXPORT const unsigned char FeedParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedParser/PublicHeader.h>
+
+
