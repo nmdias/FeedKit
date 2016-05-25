9b656ab9dbdc00c321aac0b6bddba5b39d2a4786
Nuno Dias <nmdias.pt@gmail.com>
2016-05-25 20:51:42 +0100

    Added and improved documentation for the Dublin Core module

--- Sources/Model/Modules/DublinCore.swift
+++ Sources/Model/Modules/DublinCore.swift
@@ -26,58 +26,177 @@ import Foundation
 
 /**
  
-    The Dublin Core namespace supports the Dublin Core Metadata Initiative, a 
-    standard for describing resources on the Internet to identify their author, 
-    date of publication, publisher and similar information.
+ The Dublin Core Metadata Element Set is a standard for cross-domain
+ resource description.
  
-    See: http://www.rssboard.org/rss-profile#namespace-elements-dublin
+ See https://tools.ietf.org/html/rfc5013
  
 */
 protocol DublinCoreProtocol {
     
-    /// A name given to the resource.
+    /** 
+     
+     A name given to the resource 
+     
+     */
     var dcTitle: String? { get set }
     
-    /// An entity primarily responsible for making the content of the resource.
+    /** 
+     
+     An entity primarily responsible for making the content of the resource 
+     
+     Examples of a Creator include a person, an organization, or a service. 
+     Typically, the name of a Creator should be used to indicate the entity.
+     
+     */
     var dcCreator: String? { get set }
     
-    /// The topic of the content of the resource.
+    /** 
+     
+     The topic of the content of the resource
+     
+     Typically, the subject will be represented using keywords, key phrases, 
+     or classification codes. Recommended best practice is to use a controlled 
+     vocabulary.  To describe the spatial or temporal topic of the resource, 
+     use the Coverage element.
+     
+     */
     var dcSubject: String? { get set }
     
-    /// An account of the content of the resource.
+    /** 
+     
+     An account of the content of the resource
+     
+     Description may include but is not limited to: an abstract, a table of 
+     contents, a graphical representation, or a free-text account of the 
+     resource.
+     
+     */
     var dcDescription: String? { get set }
     
-    /// An entity responsible for making the resource available.
+    /** 
+     
+     An entity responsible for making the resource available
+     
+     Examples of a Publisher include a person, an organization, or a service. 
+     Typically, the name of a Publisher should be used to indicate the entity.
+     
+     */
     var dcPublisher: String? { get set }
     
-    /// An entity responsible for making contributions to the content of the resource.
+    /** 
+     
+     An entity responsible for making contributions to the content of the
+     resource 
+     
+     Examples of a Contributor include a person, an organization, or a service.
+     Typically, the name of a Contributor should be used to indicate the entity.
+     
+     */
     var dcContributor: String? { get set }
     
-    /// A date associated with an event in the life cycle of the resource.
+    /** 
+     
+     A point or period of time associated with an event in the lifecycle of the
+     resource.
+     
+     Date may be used to express temporal information at any level of 
+     granularity. Recommended best practice is to use an encoding scheme, such
+     as the W3CDTF profile of ISO 8601 [W3CDTF].
+     
+     */
     var dcDate: String? { get set }
     
-    /// The nature or genre of the content of the resource.
+    /** 
+     
+     The nature or genre of the content of the resource
+     
+     Recommended best practice is to use a controlled vocabulary such as the 
+     DCMI Type Vocabulary [DCTYPE].  To describe the file format, physical 
+     medium, or dimensions of the resource, use the Format element.
+     
+     */
     var dcType: String? { get set }
     
-    /// The physical or digital manifestation of the resource.
+    /** 
+     
+     The file format, physical medium, or dimensions of the resource.
+     
+     Examples of dimensions include size and duration. Recommended best 
+     practice is to use a controlled vocabulary such as the list of Internet 
+     Media Types [MIME].
+     
+     */
     var dcFormat: String? { get set }
     
-    /// An unambiguous reference to the resource within a given context.
+    /** 
+     
+     An unambiguous reference to the resource within a given context.
+     
+     Recommended best practice is to identify the resource by means of a string
+     conforming to a formal identification system.
+     
+     */
     var dcIdentifier: String? { get set }
     
-    /// A Reference to a resource from which the present resource is derived.
+    /** 
+     
+     A Reference to a resource from which the present resource is derived
+     
+     The described resource may be derived from the related resource in whole
+     or in part. Recommended best practice is to identify the related resource
+     by means of a string conforming to a formal identification system.
+     
+     */
     var dcSource: String? { get set }
     
-    /// A language of the intellectual content of the resource.
+    /** 
+     
+     A language of the resource.
+     
+     Recommended best practice is to use a controlled vocabulary such as 
+     RFC 4646 [RFC4646].
+     
+     */
     var dcLanguage: String? { get set }
     
-    /// A reference to a related resource.
+    /** 
+     
+     A related resource.
+     
+     Recommended best practice is to identify the related resource by means of
+     a string conforming to a formal identification system.
+     
+     */
     var dcRelation: String? { get set }
     
-    /// The extent or scope of the content of the resource.
+    /** 
+     
+     The spatial or temporal topic of the resource, the spatial applicability
+     of the resource, or the jurisdiction under which the resource is 
+     relevant.
+     
+     Spatial topic and spatial applicability may be a named place or a location
+     specified by its geographic coordinates.  Temporal topic may be a named 
+     period, date, or date range.  A jurisdiction may be a named administrative
+     entity or a geographic place to which the resource applies.  Recommended 
+     best practice is to use a controlled vocabulary such as the Thesaurus of 
+     Geographic Names [TGN].  Where appropriate, named places or time periods 
+     can be used in preference to numeric identifiers such as sets of 
+     coordinates or date ranges.
+     
+     */
     var dcCoverage: String? { get set }
     
-    /// Information about rights held in and over the resource.
+    /**
+     
+     Information about rights held in and over the resource.
+     
+     Typically, rights information includes a statement about various property
+     rights associated with the resource, including intellectual property
+     rights.
+     
+     */
     var dcRights: String? { get set }
     
 }
--- Sources/Model/RSS/RSS2FeedChannel.swift
+++ Sources/Model/RSS/RSS2FeedChannel.swift
@@ -316,51 +316,170 @@ public class RSS2FeedChannel: SyndicationProtocol, DublinCoreProtocol {
     
     
     // MARK: - Dublin Core Module
+    /**
+     
+     A name given to the resource
+     
+     */
+    public var dcTitle: String? 
     
-    /// A name given to the resource.
-    public var dcTitle: String?
+    /**
+     
+     An entity primarily responsible for making the content of the resource
+     
+     Examples of a Creator include a person, an organization, or a service.
+     Typically, the name of a Creator should be used to indicate the entity.
+     
+     */
+    public var dcCreator: String? 
     
-    /// An entity primarily responsible for making the content of the resource.
-    public var dcCreator: String?
+    /**
+     
+     The topic of the content of the resource
+     
+     Typically, the subject will be represented using keywords, key phrases,
+     or classification codes. Recommended best practice is to use a controlled
+     vocabulary.  To describe the spatial or temporal topic of the resource,
+     use the Coverage element.
+     
+     */
+    public var dcSubject: String? 
     
-    /// The topic of the content of the resource.
-    public var dcSubject: String?
+    /**
+     
+     An account of the content of the resource
+     
+     Description may include but is not limited to: an abstract, a table of
+     contents, a graphical representation, or a free-text account of the
+     resource.
+     
+     */
+    public var dcDescription: String? 
     
-    /// An account of the content of the resource.
-    public var dcDescription: String?
+    /**
+     
+     An entity responsible for making the resource available
+     
+     Examples of a Publisher include a person, an organization, or a service.
+     Typically, the name of a Publisher should be used to indicate the entity.
+     
+     */
+    public var dcPublisher: String? 
     
-    /// An entity responsible for making the resource available.
-    public var dcPublisher: String?
+    /**
+     
+     An entity responsible for making contributions to the content of the
+     resource
+     
+     Examples of a Contributor include a person, an organization, or a service.
+     Typically, the name of a Contributor should be used to indicate the entity.
+     
+     */
+    public var dcContributor: String? 
     
-    /// An entity responsible for making contributions to the content of the resource.
-    public var dcContributor: String?
+    /**
+     
+     A point or period of time associated with an event in the lifecycle of the
+     resource.
+     
+     Date may be used to express temporal information at any level of
+     granularity. Recommended best practice is to use an encoding scheme, such
+     as the W3CDTF profile of ISO 8601 [W3CDTF].
+     
+     */
+    public var dcDate: String? 
     
-    /// A date associated with an event in the life cycle of the resource.
-    public var dcDate: String?
+    /**
+     
+     The nature or genre of the content of the resource
+     
+     Recommended best practice is to use a controlled vocabulary such as the
+     DCMI Type Vocabulary [DCTYPE].  To describe the file format, physical
+     medium, or dimensions of the resource, use the Format element.
+     
+     */
+    public var dcType: String? 
     
-    /// The nature or genre of the content of the resource.
-    public var dcType: String?
+    /**
+     
+     The file format, physical medium, or dimensions of the resource.
+     
+     Examples of dimensions include size and duration. Recommended best
+     practice is to use a controlled vocabulary such as the list of Internet
+     Media Types [MIME].
+     
+     */
+    public var dcFormat: String? 
     
-    /// The physical or digital manifestation of the resource.
-    public var dcFormat: String?
+    /**
+     
+     An unambiguous reference to the resource within a given context.
+     
+     Recommended best practice is to identify the resource by means of a string
+     conforming to a formal identification system.
+     
+     */
+    public var dcIdentifier: String? 
     
-    /// An unambiguous reference to the resource within a given context.
-    public var dcIdentifier: String?
+    /**
+     
+     A Reference to a resource from which the present resource is derived
+     
+     The described resource may be derived from the related resource in whole
+     or in part. Recommended best practice is to identify the related resource
+     by means of a string conforming to a formal identification system.
+     
+     */
+    public var dcSource: String? 
     
-    /// A Reference to a resource from which the present resource is derived.
-    public var dcSource: String?
+    /**
+     
+     A language of the resource.
+     
+     Recommended best practice is to use a controlled vocabulary such as
+     RFC 4646 [RFC4646].
+     
+     */
+    public var dcLanguage: String? 
     
-    /// A language of the intellectual content of the resource.
-    public var dcLanguage: String?
+    /**
+     
+     A related resource.
+     
+     Recommended best practice is to identify the related resource by means of
+     a string conforming to a formal identification system.
+     
+     */
+    public var dcRelation: String? 
     
-    /// A reference to a related resource.
-    public var dcRelation: String?
+    /**
+     
+     The spatial or temporal topic of the resource, the spatial applicability
+     of the resource, or the jurisdiction under which the resource is
+     relevant.
+     
+     Spatial topic and spatial applicability may be a named place or a location
+     specified by its geographic coordinates.  Temporal topic may be a named
+     period, date, or date range.  A jurisdiction may be a named administrative
+     entity or a geographic place to which the resource applies.  Recommended
+     best practice is to use a controlled vocabulary such as the Thesaurus of
+     Geographic Names [TGN].  Where appropriate, named places or time periods
+     can be used in preference to numeric identifiers such as sets of
+     coordinates or date ranges.
+     
+     */
+    public var dcCoverage: String? 
     
-    /// The extent or scope of the content of the resource.
-    public var dcCoverage: String?
-    
-    /// Information about rights held in and over the resource.
-    public var dcRights: String?
+    /**
+     
+     Information about rights held in and over the resource.
+     
+     Typically, rights information includes a statement about various property
+     rights associated with the resource, including intellectual property
+     rights.
+     
+     */
+    public var dcRights: String? 
 
     
     // MARK: - Initializers
