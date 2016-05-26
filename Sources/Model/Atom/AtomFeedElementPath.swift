2ed5c9c5fdadb36a086b23e0a3ff5d35174c1e30
Nuno Dias <nmdias.pt@gmail.com>
2016-05-26 21:24:54 +0100

    Adding the AtomFeedElementPath.swift

---
+++ Sources/Model/Atom/AtomFeedElementPath.swift
@@ -0,0 +1,48 @@
+//
+//  AtomFeedElementPath.swift
+//
+//  Copyright (c) 2016 Nuno Manuel Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+/**
+ 
+ Describes the individual path for each XML DOM element of an Atom feed
+ 
+ See https://tools.ietf.org/html/rfc4287
+ 
+ */
+enum AtomFeedElementPath: String {
+    
+    case Feed                               = "/feed"
+    
+    
+    /**
+     
+     Describes the XML DOM path to the specified node.
+     
+     */
+    var path: String {
+        return self.rawValue
+    }
+    
+}
