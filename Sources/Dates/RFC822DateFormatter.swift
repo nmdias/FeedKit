e8bccfadce6b9a42890a52b9e91c16878fa6810f
Nuno Dias <nmdias.pt@gmail.com>
2016-06-13 15:24:09 +0100

    Added RFC822DateFormatter

---
+++ Sources/Dates/RFC822DateFormatter.swift
@@ -0,0 +1,60 @@
+//
+//  RFC822DateFormatter.swift
+//
+//  Copyright (c) 2016 Nuno Manuel Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+/**
+
+ Converts date and time textual representations within the RFC822
+ date specification into `NSDate` objects
+ 
+ */
+class RFC822DateFormatter: NSDateFormatter {
+    
+    let dateFormats = [
+        "EEE, d MMM yyyy HH:mm:ss zzz",
+        "EEE, d MMM yyyy HH:mm zzz"
+    ]
+    
+    override init() {
+        super.init()
+        self.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        self.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func dateFromString(string: String) -> NSDate? {
+        for dateFormat in self.dateFormats {
+            self.dateFormat = dateFormat
+            if let date = super.dateFromString(string) {
+                return date
+            }
+        }
+        return nil
+    }
+    
+}
