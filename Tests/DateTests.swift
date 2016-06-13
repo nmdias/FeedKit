45d6a56721aacbe49e55ddb99861af21bb38a3bb
Nuno Dias <nmdias.pt@gmail.com>
2016-06-13 15:27:17 +0100

    Added date unit tests

---
+++ Tests/DateTests.swift
@@ -0,0 +1,146 @@
+//
+//  DateTests.swift
+//
+//  Copyright (c) 2016 Nuno Manuel Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import XCTest
+@testable import FeedParser
+
+class DateTests: BaseTestCase {
+    
+    func testRFC822DateFormatter() {
+        
+        // Given
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let rfc822DateFormatter = RFC822DateFormatter()
+        let dateString = "Tue, 04 Feb 2014 22:03:45 Z"
+        
+        // When
+        let date = rfc822DateFormatter.dateFromString(dateString)
+        
+        // Then
+        XCTAssertNotNil(date)
+        
+        let components = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second, .TimeZone, .Calendar], fromDate: date!)
+        
+        XCTAssertEqual(components.day, 4)
+        XCTAssertEqual(components.month, 2)
+        XCTAssertEqual(components.year, 2014)
+        XCTAssertEqual(components.hour, 22)
+        XCTAssertEqual(components.minute, 3)
+        XCTAssertEqual(components.second, 45)
+        XCTAssertEqual(components.timeZone?.daylightSavingTimeOffset, 3600)
+        XCTAssertEqual(components.timeZone?.secondsFromGMT, 3600)
+        XCTAssertEqual(components.calendar?.calendarIdentifier, NSCalendarIdentifierGregorian)
+        
+    }
+    
+    func testRFC3339DateFormatter() {
+        
+        // Given
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let rfc3339DateFormatter = RFC3339DateFormatter()
+        let dateString = "2016-01-15T15:54:10-01:00"
+        
+        // When
+        let date = rfc3339DateFormatter.dateFromString(dateString)
+        
+        // Then
+        XCTAssertNotNil(date)
+        
+        let components = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second, .TimeZone, .Calendar], fromDate: date!)
+        
+        XCTAssertEqual(components.day, 15)
+        XCTAssertEqual(components.month, 1)
+        XCTAssertEqual(components.year, 2016)
+        XCTAssertEqual(components.hour, 16)
+        XCTAssertEqual(components.minute, 54)
+        XCTAssertEqual(components.second, 10)
+        XCTAssertEqual(components.timeZone?.daylightSavingTimeOffset, 3600)
+        XCTAssertEqual(components.timeZone?.secondsFromGMT, 3600)
+        XCTAssertEqual(components.calendar?.calendarIdentifier, NSCalendarIdentifierGregorian)
+        
+    }
+    
+    func testDateFromRFC822Spec() {
+        
+        // Given
+        let spec = DateSpec.RFC822
+        let dateStrings = [
+            "Tue, 04 Feb 2014 22:10:15 Z",
+            "Sun, 05 Jun 2016 08:35:14 Z",
+            "Sun, 05 Jun 2016 08:54 +0000",
+            "Mon, 21 Mar 2016 13:31:23 GMT",
+            "Sat, 04 Jun 2016 16:26:37 EDT",
+            "Sat, 04 Jun 2016 11:55:28 PDT",
+            "Sun, 5 Jun 2016 01:51:07 -0700",
+            "Sun, 5 Jun 2016 01:30:30 -0700",
+            "Thu, 02 June 2016 14:43:37 GMT",
+            "Sun, 5 Jun 2016 01:49:56 -0700",
+            "Fri, 27 May 2016 14:32:19 -0400",
+            "Sun, 05 Jun 2016 01:45:00 -0700",
+            "Sun, 05 Jun 2016 08:32:03 +0000",
+            "Sat, 04 Jun 2016 22:33:02 +0000",
+            "Sun, 05 Jun 2016 01:52:30 -0700",
+            "Thu, 02 Jun 2016 15:24:37 +0000"
+        ]
+        
+        // When
+        let dates = dateStrings.flatMap { (dateString) -> NSDate? in
+            return dateString.dateFromSpec(spec)
+        }
+        
+        // Then
+        XCTAssertEqual(dateStrings.count, dates.count)
+        
+    }
+    
+    func testDateFromRFC3339Spec() {
+        
+        // Given
+        let spec = DateSpec.RFC3999
+        let dateStrings = [
+            "2016-06-05T09:30:01Z",
+            "2016-06-05T03:18:00Z",
+            "2016-06-05T04:30:29Z",
+            "1985-04-12T23:20:50.52Z",
+            "2016-06-02T18:07:16+01:00",
+            "2016-06-05T05:00:03-04:00",
+            "2016-04-30T11:51:54-04:00",
+            "2016-06-03T17:01:48-07:00",
+            "2016-06-05T03:12:10+00:00",
+            "2015-02-22T13:13:48-05:00",
+            "2016-06-03T23:33:00-04:00",
+            "1996-12-19T16:39:57-08:00",
+        ]
+        
+        // When
+        let dates = dateStrings.flatMap { (dateString) -> NSDate? in
+            return dateString.dateFromSpec(spec)
+        }
+        
+        // Then
+        XCTAssertEqual(dateStrings.count, dates.count)
+        
+    }
+    
+}
