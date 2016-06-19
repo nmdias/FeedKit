6be12883f3eff48e1059817abb14435262ef9d3b
Nuno Dias <nmdias.pt@gmail.com>
2016-06-19 09:58:53 +0100

    Added macOS example

--- Example macOS/AppDelegate.swift
+++ Example macOS/AppDelegate.swift
@@ -27,9 +27,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
-
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
---
+++ Example macOS/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//
+//  Copyright (c) 2016 Nuno Manuel Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Cocoa
+import FeedParser
+
+let feedURL = NSURL(string: "http://images.apple.com/main/rss/hotnews/hotnews.rss")!
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var feedTableView: NSTableView!
+    @IBOutlet weak var feedItemsTableView: NSTableView!
+    @IBOutlet var textView: NSTextView!
+    
+    var feed: RSSFeed?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        FeedParser(URL: feedURL)!.parse { (result) in
+            self.feed = result.rssFeed
+            self.feedItemsTableView.reloadData()
+        }
+        
+    }
+
+    // MARK: - Text View Helper
+    
+    func updateTextView() {
+        let selectedRow = self.feedItemsTableView.selectedRow
+        if selectedRow > -1 {
+            let item = self.feed!.items![selectedRow]
+            self.textView.string = item.description
+        }
+    }
+    
+}
+
+// MARK: - Table View Delegate
+
+extension ViewController: NSTableViewDelegate {
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        guard let cell = tableView.makeViewWithIdentifier("Cell", owner: nil) as? NSTableCellView else {
+            return nil
+        }
+        
+        switch tableView {
+            
+        case self.feedTableView:
+            switch row {
+            case 0: cell.textField?.stringValue = self.feed?.title ?? "[no title]"
+            case 1: cell.textField?.stringValue = self.feed?.link ?? "[no link]"
+            case 2: cell.textField?.stringValue = self.feed?.description ?? "[no description]"
+            default: fatalError()
+            }
+            
+        case self.feedItemsTableView:
+            cell.textField?.stringValue = self.feed!.items![row].title ?? "[no title]"
+            
+        default: fatalError()
+            
+        }
+        
+        return cell
+        
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        self.updateTextView()
+    }
+    
+}
+
+// MARK: - Table View Data Source
+
+extension ViewController: NSTableViewDataSource {
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        switch tableView {
+        case self.feedTableView: return 3
+        case self.feedItemsTableView: return self.feed?.items?.count ?? 0
+        default: fatalError()
+        }
+    }
+    
+}
