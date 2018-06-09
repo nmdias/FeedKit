ff34e0affd3e1f882cbb89434fde8cdd1a34df43
Nuno Dias <nmdias.pt@gmail.com>
2018-06-09 21:35:32 +0100

    Updated copyright dates

--- Sources/FeedKit/Models/Atom/AtomFeedAuthor.swift
+++ Sources/FeedKit/Models/Atom/AtomFeedAuthor.swift
@@ -1,7 +1,7 @@
 //
 //  AtomFeedAuthor.swift
 //
-//  Copyright (c) 2017 Nuno Manuel Dias
+//  Copyright (c) 2016 - 2018 Nuno Manuel Dias
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
