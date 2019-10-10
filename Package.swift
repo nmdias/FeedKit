a0f1cc5df28bda431f451a3fa3f10805b018064d
nmdias <nmdias.pt@gmail.com>
2019-10-10 16:19:20 +0100

    Updated Package.swift to require Swift 5+

--- Package.swift
+++ Package.swift
@@ -1,13 +1,22 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "FeedKit",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
         .library(name: "FeedKit", targets: ["FeedKit"]),
     ],
     targets: [
         .target(name: "FeedKit", dependencies: []),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
