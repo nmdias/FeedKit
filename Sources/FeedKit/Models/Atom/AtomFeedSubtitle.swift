c21e9dff28f405c3d968b3c69341ddf473da5a63
Nuno Dias <nmdias@users.noreply.github.com>
2024-11-09 12:42:05 +0100

    Add AtomFeed model

---
+++ Sources/FeedKit/Models/Atom/AtomFeedSubtitle.swift
@@ -0,0 +1,44 @@
+//
+//  AtomFeedSubtitle.swift
+//
+//  Copyright (c) 2016 - 2024 Nuno Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+/// The "atom:subtitle" element is a Text construct that conveys a human-
+/// readable description or subtitle for a feed.
+public struct AtomFeedSubtitle: Codable {
+  /// The element's text.
+  public var text: String?
+
+  /// The element's attributes.
+  public struct Attributes: Codable {
+    /// Text constructs MAY have a "type" attribute.  When present, the value
+    /// MUST be one of "text", "html", or "xhtml".  If the "type" attribute
+    /// is not provided, Atom Processors MUST behave as though it were
+    /// present with a value of "text".
+    public var type: String?
+  }
+
+  /// The element's attributes.
+  public var attributes: Attributes?
+}
---
+++ Sources/FeedKit/Models/Atom/AtomFeedSummary.swift
@@ -0,0 +1,50 @@
+//
+//  AtomFeedSummary.swift
+//
+//  Copyright (c) 2016 - 2024 Nuno Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+/// The "atom:summary" element is a Text construct that conveys a short
+/// summary, abstract, or excerpt of an enactry.
+///
+/// atomSummary = element atom:summary { atomTextConstruct }
+///
+/// It is not advisable for the atom:summary element to duplicate
+/// atom:title or atom:content because Atom Processors might assume there
+/// is a useful summary when there is none.
+public struct AtomFeedSummary: Codable {
+  /// The element's text.
+  public var text: String?
+
+  /// The element's attributes.
+  public struct Attributes: Codable {
+    /// Text constructs MAY have a "type" attribute.  When present, the value
+    /// MUST be one of "text", "html", or "xhtml".  If the "type" attribute
+    /// is not provided, Atom Processors MUST behave as though it were
+    /// present with a value of "text".
+    public var type: String?
+  }
+
+  /// The element's attributes.
+  public var attributes: Attributes?
+}
