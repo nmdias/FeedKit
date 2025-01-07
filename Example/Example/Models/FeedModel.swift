ec572de0c15d11dfe0c241dd9d8c5ab885580b9b
Nuno Dias <nmdias@users.noreply.github.com>
2025-01-07 03:25:39 +0100

    Remove FeedItemModel from FeedModel file and update license header

--- Example/Example/Models/FeedModel.swift
+++ Example/Example/Models/FeedModel.swift
@@ -1,37 +1,29 @@
 //
-//  FeedItemModel.swift
-//  Example
+//  FeedModel.swift
 //
-//  Created by Nuno Dias on 07/01/2025.
+//  Copyright (c) 2016 - 2024 Nuno Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
-
-struct FeedItemModel: Equatable {
-  var title: String?
-  var description: String?
-  var date: Date?
-
-  init() {
-  }
-
-  init(item: RSSFeedItem) {
-    title = item.title
-    description = item.description
-    date = item.pubDate
-  }
-
-  init(item: AtomFeedEntry) {
-    title = item.title
-    description = item.summary?.text
-    date = item.published
-  }
-
-  init(item: JSONFeedItem) {
-    title = item.title
-    description = item.summary
-    date = item.datePublished
-  }
-}
+import FeedKit
+import Foundation
 
 struct FeedModel: Equatable {
   var title: String?
@@ -39,8 +31,7 @@ struct FeedModel: Equatable {
   var description: String?
   var items: [FeedItemModel]?
 
-  init() {
-  }
+  init() {}
 
   init(feed: AtomFeed) {
     title = feed.title?.text
