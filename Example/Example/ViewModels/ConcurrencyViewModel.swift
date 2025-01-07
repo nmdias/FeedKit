2ae88cc288afb4a0cd4d7a14251252576c63f528
Nuno Dias <nmdias@users.noreply.github.com>
2025-01-07 03:27:35 +0100

    Add licence header to ConcurrencyViewModel and update urlStrings naming

--- Example/Example/ViewModels/ConcurrencyViewModel.swift
+++ Example/Example/ViewModels/ConcurrencyViewModel.swift
@@ -1,10 +1,37 @@
+//
+//  ConcurrencyViewModel.swift
+//
+//  Copyright (c) 2016 - 2024 Nuno Dias
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import FeedKit
+import SwiftUI
+
 @MainActor
 class ConcurrencyViewModel: ObservableObject {
   @Published var feeds: [FeedModel] = []
 
   func loadFeeds() async {
     feeds = []
-    await FeedParser().parse(from: urlStrings) { [weak self] result in
+    await FeedParser().parse(from: feedUrls) { [weak self] result in
       Task { @MainActor in
 
         switch result {
@@ -18,7 +45,7 @@ class ConcurrencyViewModel: ObservableObject {
     }
   }
 
-  let urlStrings = [
+  let feedUrls = [
     "https://techcrunch.com/feed/atom/",
     "https://www.9to5mac.com/feed/",
     "https://www.anandtech.com/rss",
