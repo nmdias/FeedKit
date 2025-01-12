314f5ffeab6c86a7081c06a2d6ddf830b21ffbe0
Nuno Dias <nmdias@users.noreply.github.com>
2025-01-12 17:33:58 +0100

    Update copyright header dates

--- Sources/FeedKit/FeedType.swift
+++ Sources/FeedKit/FeedType.swift
@@ -1,7 +1,7 @@
 //
 //  FeedType.swift
 //
-//  Copyright (c) 2016 - 2024 Nuno Manuel Dias
+//  Copyright (c) 2016 - 2025 Nuno Dias
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
