59a0a93e8a8566cb01fd655b86ed79fd934842ec
Nuno Dias <nmdias@users.noreply.github.com>
2025-01-13 01:17:59 +0100

    Create Package@swift-5.8.swift

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,68 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "FeedKit",
+  platforms: [
+    .macOS(.v12),
+    .iOS(.v15),
+    .watchOS(.v8),
+    .tvOS(.v15),
+  ],
+  products: [
+    .library(
+      name: "FeedKit",
+      targets: [
+        "FeedKit"
+      ]
+    ),
+    .library(
+      name: "XMLKit",
+      targets: [
+        "XMLKit"
+      ]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "XMLKit"
+    ),
+    .testTarget(
+      name: "XMLKitTests",
+      dependencies: ["XMLKit"],
+      resources: [
+        .process("Resources/xml/Sample.xml")
+      ]
+    ),
+    .target(
+      name: "FeedKit",
+      dependencies: [
+        "XMLKit"
+      ]
+    ),
+    .testTarget(
+      name: "FeedKitTests",
+      dependencies: [
+        "FeedKit"
+      ],
+      resources: [
+        .process("Resources/json/feed.json"),
+        .process("Resources/xml/Ampersand.xml"),
+        .process("Resources/xml/Atom + XHTML.xml"),
+        .process("Resources/xml/Atom.xml"),
+        .process("Resources/xml/AtomMedia.xml"),
+        .process("Resources/xml/Content.xml"),
+        .process("Resources/xml/FeedNotFound.xml"),
+        .process("Resources/xml/RSS.xml"),
+        .process("Resources/xml/RSSDC.xml"),
+        .process("Resources/xml/RSSAtom.xml"),
+        .process("Resources/xml/Media.xml"),
+        .process("Resources/xml/Syndication.xml"),
+        .process("Resources/xml/iTunes.xml"),
+        .process("Resources/xml/YouTube.xml"),
+      ]
+    ),
+  ]
+)
