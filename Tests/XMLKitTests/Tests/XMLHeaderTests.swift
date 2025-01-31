a11cadd273c3b00bf238ef796cc5252fab39293f
Nuno Dias <nmdias@users.noreply.github.com>
2025-01-31 23:54:25 +0100

    Create XMLHeaderTests.swift

---
+++ Tests/XMLKitTests/Tests/XMLHeaderTests.swift
@@ -0,0 +1,88 @@
+//
+// XMLHeaderTests.swift
+//
+// Copyright (c) 2016 - 2025 Nuno Dias
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Testing
+import XMLKit
+
+@Suite("XMLHeader")
+struct XMLHeaderTests: XMLKitTestable {
+  @Test
+  func `default`() throws {
+    // Given
+    let header: XMLHeader = .default
+    let expected = """
+    <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+    """
+
+    // When
+    let actual = header.toXMLString()
+
+    // Then
+    #expect(expected == actual)
+  }
+
+  @Test
+  func version() throws {
+    // Given
+    let header: XMLHeader = .init(version: "1.1")
+    let expected = """
+    <?xml version=\"1.1\" encoding=\"UTF-8\"?>
+    """
+
+    // When
+    let actual = header.toXMLString()
+
+    // Then
+    #expect(expected == actual)
+  }
+
+  @Test
+  func encoding() throws {
+    // Given
+    let header: XMLHeader = .init(encoding: .utf16)
+    let expected = """
+    <?xml version=\"1.0\" encoding=\"UTF-16\"?>
+    """
+
+    // When
+    let actual = header.toXMLString()
+
+    // Then
+    #expect(expected == actual)
+  }
+
+  @Test
+  func isStandalone() throws {
+    // Given
+    let header: XMLHeader = .init(version: "1.0", isStandalone: true)
+    let expected = """
+    <?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+    """
+
+    // When
+    let actual = header.toXMLString()
+
+    // Then
+    #expect(expected == actual)
+  }
+}
